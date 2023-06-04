0c057b1a5d38389d951d69578d297ae2798f768f
Anthony Catullo <anthonycatullo@oatslam.local>
2023-06-04 13:02:22 -0500

    added create post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,12 +8,23 @@ import Image from "next/image";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { LoadingPage } from "~/components/Loading";
+import { useState, useContext } from "react";
 
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const { user } = useUser();
   if (!user) return null;
+  const [input, setInput] = useState("");
+
+  const ctx = api.useContext();
+
+  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
+    onSuccess: () => {
+      setInput("");
+      void ctx.posts.getAll.invalidate();
+    },
+  });
 
   return (
     <div className="flex w-full gap-3">
@@ -27,7 +38,12 @@ const CreatePostWizard = () => {
       <input
         placeholder="Type some emojis!"
         className="grow bg-transparent outline-none"
+        value={input}
+        type="text"
+        onChange={(e) => setInput(e.target.value)}
+        disabled={isPosting}
       />
+      <button onClick={() => mutate({ content: input })}>Post</button>
     </div>
   );
 };
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,7 +1,7 @@
 import { clerkClient } from "@clerk/nextjs";
 import type { User } from "@clerk/nextjs/dist/types/server";
 import { z } from "zod";
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
 import { TRPCError } from "@trpc/server";
 
 
@@ -17,6 +17,7 @@ export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.prisma.post.findMany({
       take: 100,
+      orderBy: [{ createdAt: "desc" }],
     });
     
     const users = (
@@ -38,7 +39,26 @@ export const postsRouter = createTRPCRouter({
           username: author.username
         },
     }
-  })})
+  })}),
+
+  create: privateProcedure
+  .input(
+    z.object({
+      content: z.string().emoji().min(1).max(280),
+    })
+  )
+  .mutation(async ({ ctx, input }) => {
+    const authorId = ctx.userId;
+
+    const post = await ctx.prisma.post.create({
+      data: {
+        authorId,
+        content: input.content,
+      },
+    });
+
+    return post;
+  }),
 })
 
 
